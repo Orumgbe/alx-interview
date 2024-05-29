9cba01397b356fea1e3e1bc6f0d2aa68e5956f27
Orumgbe <orumgbeugbekile@gmail.com>
2024-05-29 08:27:43 -0700

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    """Initialize the set of opened boxes and the list of keys we have"""
+    opened_boxes = set([0])
+    keys = boxes[0]
+
+    # Iterate while we have keys to process
+    while keys:
+        key = keys.pop()
+        if key not in opened_boxes and key < len(boxes):
+            opened_boxes.add(key)
+            keys.extend(boxes[key])
+
+    # Check if we've opened all boxes
+    return len(opened_boxes) == len(boxes)
